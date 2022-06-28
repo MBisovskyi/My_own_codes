17bab36322ddedeb8a06697f9819571954847672
unknown <mbisovskyi@gmail.com>
2022-06-28 09:52:24 -0400

    Little cosmetology for the code

--- problem_solving_pt2.py
+++ problem_solving_pt2.py
@@ -3,24 +3,6 @@ import math
 #       b. A happy number is a number defined by the following process: starting with any positive integer, replace the number by the sum of the squares of its digits, and repeat the process until the number equals 1. An example of a happy number is 19
 #       c. Write a method that determines if a number is happy or sad         
 
-# def happy_number():
-#     squared_input_number = int(input("Enter a number: "))**2 # Square of input number
-#     string_numbers = str(squared_input_number)
-#     list_of_string_numbers = []
-#     is_happy_number = False
-#     while is_happy_number == False:    
-#         for str_number in string_numbers:
-#             list_of_string_numbers.append(str_number)
-#         list_length = len(list_of_string_numbers)
-#         index = 0
-#         sum_of_squared_numbers = int(list_of_string_numbers[index])**2 + int(list_of_string_numbers[index + 1])**2
-#         if sum_of_squared_numbers == 1:
-#             is_happy_number = True
-#         else:
-#             list_of_string_numbers.clear()
-#             string_numbers = str(sum_of_squared_numbers)
-#     print("Number is happy!")
-
 def happy_number():
     input_number = input("Enter a number: ")
     sum_of_numbers = 0
@@ -44,7 +26,6 @@ def happy_number():
         elif sum_of_numbers == peak_number:
             print(f"Number {input_number} is sad!")
             break
-
 
 happy_number()
 
